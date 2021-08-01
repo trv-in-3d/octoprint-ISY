8fe12e1797f80ddfbeb0e1d823a5db2b6f090a8d
trv-in-3d <48891694+trv-in-3d@users.noreply.github.com>
2021-08-01 17:53:23 -0400

    Start Here
    Initial code base.

---
+++ README.md
@@ -0,0 +1,46 @@
+# OctoPrint-ISY
+
+Connects OctoPrint events to ISY.
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
+or manually using this URL:
+
+    https://github.com/tjjfvi/OctoPrint-ISY/archive/master.zip
+
+Then follow the steps listed [here](https://github.com/tjjfvi/OctoPrint-ISY/wiki).
+
+## Configuration
+
+### Makerkeys
+A unique API key for ISY. Go [here](https://IFTT.com/services/maker_webhooks) and click `Documentation`. Look at the URL it sends you to; the last part (after the `/use/`) is the makerkey. You can put multiple makerkeys, seperated by newlines, here.
+
+### Default prefixes
+Default prefixes for the triggers. If you have an event `MyEvent` and prefixes `prefix1-` and `prefix2-`, it will, by default make the triggers `prefix1-MyEvent` and `prefix2-MyEvent`. Seperate the prefixes with newlines.
+
+### Events
+Define events to send to ISY.
+
+#### Triggers
+A list of triggers to trigger on ISY.
+
+#### Values
+ISY Webhooks allows for a payload with three values. It will interpret this string like so:
+1. If the value begins with a dot (`.`), it will use that prop of the event payload (e.g. `.name` for `PrintDone`)
+1. If it begins with an at symbol (`@`):
+   1. `path` will be the result of interpreting the string with the first two characters removed as a value
+   1. If the second character in the value is an `f` it will prepend to `path` the base uploads folder
+   1. Otherwise, the second character should be a dash (`-`)
+   1. It will then upload the file at `path` to [file.io](https://file.io) and return a link to that file.
+1. If it begins with a colon (`:`), it will use the string after the colon
+1. If it begins with a dollar sign (`$`), it will:
+   1. If the second character is a `t`:
+      1. Let `time` be the result of interpreting the string with the first three characters removed as a value.
+      1. If the third character is `:`, it will format `time` in `H:MM` format (e.g. `1:02`).
+      1. If the third character is `$`, it will format `time` in `H:MM:SS` format (e.g. `1:02:34`)
+      1. If the third character is `h`, it will format `time` in Hh` format (e.g. `1h`)
+      1. If the third character is `m`, it will format `time` in `Hh Mm` format (e.g. `1h 2m`)
+      1. If the third character is `s`, it will format `time` in `Hh Mm Ss` format (e.g. `1h 2m 34s`)
+1. Otherwise, it will just send the plain text
+
---
+++ octoprint_ISY/__init__.py
@@ -0,0 +1,142 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint
+import requests
+
+class ISYplugin(
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.EventHandlerPlugin,
+):
+    def on_after_startup(self):
+        self._storage_interface = self._file_manager._storage("local")
+
+    def on_event(self, event_name, event_payload):
+        events = self._settings.get(["events"])
+        default_prefixes = self._settings.get(["default_prefixes"])
+        makerkeys = self._settings.get(["makerkeys"])
+
+        for event in events:
+            if event["event_name"] != event_name: continue
+
+            trigger_names = filter(lambda name: name.strip(), event["trigger_names"])
+
+            if not len(trigger_names):
+                trigger_names = [prefix + event_name for prefix in default_prefixes]
+
+            value_thunks = [self._interpret_value(event_payload, value) for value in event["values"] + [""] * (3 - len(event["values"]))]
+            #payload_thunk = lambda: { "value1": value_thunks[0](), "value2": value_thunks[1](), "value3": value_thunks[2]() }
+            payload_thunk = lambda: {value_thunks[0]()}
+            for trigger_name in trigger_names:
+                for makerkey in makerkeys:
+                    #payload = payload_thunk()
+                    payload = value_thunks[0]()
+                    #url = "https://maker.ifttt.com/trigger/%s/with/key/%s" % (trigger_name, makerkey)
+                    url = "https://my.isy.io/api/ifttt/setPrinterState/key/%s" % (makerkey)
+                    self._logger.info("sending a request to url: %s, payload: %s" % (url.replace(makerkey, "[REDACTED; %s...]" % makerkey[:8]), payload));
+
+                    response = requests.post(url, data=payload)
+                    self._logger.info("response: " + response.text)
+
+    def _interpret_value(self, payload, value):
+        to_thunk = lambda x: lambda: x
+        self._logger.info(value)
+
+        if len(value) == 0:
+            return to_thunk("")
+
+        if value[0] == ".":
+            return to_thunk(payload[value[1:]])
+
+        if value[0] == ":":
+            return to_thunk(value[1:])
+
+        if value[0] == "@":
+            path = self._interpret_value(payload, value[2:])()
+            if value[1] == "f":
+                path = self._storage_interface.path_on_disk(path)
+
+            return lambda: requests.post("https://file.io", files={ "file": open(path, "rb") }).json()["link"]
+
+        if value[0] == "$":
+            if value[1] == "t":
+                t = self._interpret_value(payload, value[3:])()
+                s = str(int(t)%60)
+                m = str(int(t/60)%60)
+                h = str(int(t/3600))
+                sf = s.zfill(2)
+                mf = m.zfill(2)
+                hf = h.zfill(2)
+                
+                if value[2] == "$":
+                    return to_thunk('%s:%s:%s' % (hf, mf, sf))
+                    
+                if value[2] == ":":
+                    return to_thunk('%s:%s' % (hf, mf))
+
+                if value[2] == "h":
+                    return to_thunk('%sh' % (h))
+
+                if value[2] == "m":
+                    return to_thunk('%sh %sm' % (h, m))
+
+                if value[2] == "s":
+                    return to_thunk('%sh %sm %ss' % (h, m, s))
+
+                return to_thunk(str(t))
+
+        return to_thunk(value)
+
+    def get_settings_defaults(self):
+        return dict(
+            makerkeys=[],
+            default_prefixes = ["OctoPrint-", "op-"],
+            events=[
+                dict(event_name="PrintDone", trigger_names=[], values=[".name", ".time", ".origin"]),
+                dict(event_name="PrintStarted", trigger_names=[], values=[".name", ".origin", ""]),
+                dict(event_name="PrintFailed", trigger_names=[], values=[".name", ".reason", ".origin"]),
+            ],
+        )
+
+    def get_settings_restricted_paths(self):
+        return dict(admin=[["makerkeys"]])
+
+    def get_template_configs(self):
+        return [dict(type="settings", custom_bindings=True)]
+
+    def get_assets(self):
+        return dict(
+            js=["js/ISY.js"],
+            css=["css/ISY.css"],
+        )
+
+    def get_update_information(self):
+        return dict(
+            ISY=dict(
+                displayName="OctoPrint-ISY",
+                displayVersion=self._plugin_version,
+
+                type="github_release",
+                user="tjjfvi",
+                repo="OctoPrint-ISY",
+                current=self._plugin_version,
+
+                pip="https://github.com/tjjfvi/OctoPrint-ISY/archive/{target_version}.zip"
+            )
+        )
+
+__plugin_name__ = "OctoPrint-ISY"
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = ISYplugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
